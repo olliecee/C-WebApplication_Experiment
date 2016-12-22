44077b3ea5c67a7c0edfdb213c98cb40f15934ac
3A46AA1 <olliecee.mail@gmail.com>
2016-12-22 13:21:36 -0500

    C# declaring objects in short and long form

---
+++ Project 2.1/CSharpTutorial/Band.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    public class Band
+    {
+        public Member Member { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ Project 2.1/CSharpTutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Long way of declaring an object
+            var band = new Band();
+            band.Bio = "Human, Female, 19, California";
+            band.Name = "Anne";
+            band.Member = new Member(band.Name);
+
+            // Short way of declaring an object
+            var band2 = new Band
+            {
+                Bio = "Human, Female, 19, California",
+                Name = "May"
+            };
+
+            Console.WriteLine(band.Name);
+            Console.WriteLine(band2.Name);
+        }
+    }
+}
