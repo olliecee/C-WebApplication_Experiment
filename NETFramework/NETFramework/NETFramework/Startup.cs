9e6db6a6591c8a099cda473a793f529977c17764
3A46AA1 <olliecee.mail@gmail.com>
2017-01-17 19:40:36 -0500

    .ASP Web Applications using .NET Framework 4.6.1 and .NET Core 1.0.x

---
+++ NETFramework/NETFramework/NETFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NETFramework.Startup))]
+namespace NETFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
