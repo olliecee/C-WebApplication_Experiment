783b48f0374c59ccf2e0353058b509c94604ce52
3A46AA1 <olliecee.mail@gmail.com>
2017-01-20 15:11:52 -0500

    Created project implementing individual authentication + models

---
+++ ASP.NET Core - MVC-PRO1/ASP.NET Core - MVC Movie = MVC/src/MVCMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
