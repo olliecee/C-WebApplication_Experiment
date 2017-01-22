4766437cd8523c1d55d8c9284fce147183b2a0d8
3A46AA1 <olliecee.mail@gmail.com>
2017-01-21 22:34:29 -0500

    Added validation in the models. Added missing information in views

--- ASP.NET Core - MVC-PRO1/ASP.NET Core - MVC Movie = MVC/src/MVCMovie/Models/Movie.cs
+++ ASP.NET Core - MVC-PRO1/ASP.NET Core - MVC Movie = MVC/src/MVCMovie/Models/Movie.cs
@@ -6,14 +6,22 @@ namespace MVCMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Display(Name = "Current Price")]
         public decimal Price { get; set; }
         [Display(Name = "Popularity")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 }
